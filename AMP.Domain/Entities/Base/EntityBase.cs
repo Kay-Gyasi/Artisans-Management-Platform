d6861f4530ca84d505a9db941af99f551992222a
Kofi Gyasi <kaygyasi715@gmail.com>
2022-06-17 13:58:00 +0000

    kay

--- AMP.Domain/Entities/Base/EntityBase.cs
+++ AMP.Domain/Entities/Base/EntityBase.cs
@@ -4,9 +4,8 @@ namespace AMP.Domain.Entities.Base
 {
     public class EntityBase
     {
-        
         public int Id { get; set; }
-        public DateTime DateCreated { get; private set; } // if id = 0, value is DateTime.UtcNow
+        public DateTime DateCreated { get; private set; } // if id = 0, value is DateTime.UtcNow (Do that in configuration file)
         public DateTime DateModified { get; private set; }
         public EntityStatus EntityStatus { get; set; } = EntityStatus.Normal;
         // Entity Status (for soft delete)
